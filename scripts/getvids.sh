c9af0c990e92bbba055f2faf8aaddd06b8fcd175
Pete Bachant <petebachant@gmail.com>
2015-11-21 15:14:51 -0500

    Use 30 fps RVAT LES video

--- scripts/getvids.sh
+++ scripts/getvids.sh
@@ -1,4 +1,4 @@
-cp "C:/Users/Pete/Google Drive/Research/OpenFOAM/turbinesFoam/video/turbinesFoam-RVAT-LES-fineMesh.ogv" videos/unh-rvat-alm-les.ogv
+cp "C:/Users/Pete/Google Drive/Research/OpenFOAM/turbinesFoam/video/unh-rvat-les-bdf45a7-6_thresh-30fps.ogv" videos/unh-rvat-alm-les.ogv
 
 cp "C:/Users/Pete/Google Drive/Research/OpenFOAM/turbinesFoam/video/unh-rvat-near-surface-1.75mps.ogv" videos/unh-rvat-alm-free-surface.ogv
 
